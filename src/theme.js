559f4e6f6d275895c4c84f90019764aa83c36a8a
Emma Litwa-Vulcu <80431903+reiniiriarios@users.noreply.github.com>
2023-04-16 17:16:41 -0700

    fix: theme set wait on dom loaded

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,9 @@
 const themeToggle = document.getElementById('theme-toggle');
-let theme = localStorage.getItem('theme') ?? 'light';
-themeToggle.getElementsByTagName('input')[0].checked = theme == 'dark';
-document.documentElement.setAttribute('data-theme', theme);
+document.addEventListener("DOMContentLoaded", () => {
+  let theme = localStorage.getItem('theme') ?? 'light';
+  themeToggle.getElementsByTagName('input')[0].checked = theme == 'dark';
+  document.documentElement.setAttribute('data-theme', theme);
+});
 
 function toggleTheme(e) {
   let theme = e.target.checked ? 'dark' : 'light';
