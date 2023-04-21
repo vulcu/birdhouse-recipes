d474137f436ac4b0a5b2c287ed24af5e9619bc4f
Emma Litwa-Vulcu <80431903+reiniiriarios@users.noreply.github.com>
2023-04-20 21:42:31 -0700

    feat: grid/list toggle

---
+++ src/js/recipe-view.js
@@ -0,0 +1,19 @@
+const recipeViewToggle = document.getElementById('recipe-view-toggle');
+const recipeLists = document.getElementsByClassName('recipe-list');
+
+function updateRecipeView(view) {
+  recipeViewToggle.setAttribute('data-view', view);
+  for (var i=0; i < recipeLists.length; i++) {
+    recipeLists.item(i).setAttribute('data-view', view);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  updateRecipeView(localStorage.getItem('recipe-view') ?? 'grid');
+});
+
+recipeViewToggle.addEventListener('click', () => {
+  var view = localStorage.getItem('recipe-view') == 'list' ? 'grid' : 'list';
+  localStorage.setItem('recipe-view', view);
+  updateRecipeView(view);
+});
