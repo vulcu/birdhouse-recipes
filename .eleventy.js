98d5ef371b54099ec3c107e031f3bb5b549f6dc3
Emma Litwa-Vulcu <80431903+reiniiriarios@users.noreply.github.com>
2023-04-20 18:24:44 -0700

    feat: add menu support

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,13 @@
 const yaml = require('js-yaml');
 const responsiveImage = require('./src/js/filters/responsive-images.js');
 const parseMiniMd = require('./src/js/filters/parse-mini-markdown.js');
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ 'src/public': '/' });
   eleventyConfig.addLayoutAlias('default', 'layout.njk');
   eleventyConfig.addDataExtension('yaml', (contents) => yaml.load(contents));
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.addFilter('md', parseMiniMd);
   eleventyConfig.addShortcode('image', responsiveImage);
 
