27b1eecd78ea6662a8acfead67babcf4e68e5ffd
Emma Litwa-Vulcu <80431903+reiniiriarios@users.noreply.github.com>
2023-04-21 14:49:22 -0700

    feat: icon shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const yaml = require('js-yaml');
 const responsiveImage = require('./src/js/shortcodes/responsive-images.js');
+const icon = require('./src/js/shortcodes/icon.js');
 const parseMiniMd = require('./src/js/filters/parse-mini-markdown.js');
 const license = require('./src/js/shortcodes/license.js');
 const photoAttribution = require('./src/js/shortcodes/photo-attribution.js');
@@ -7,11 +8,13 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ 'src/public': '/' });
+  eleventyConfig.addPassthroughCopy({ 'src/icons': '/icons/' });
   eleventyConfig.addLayoutAlias('default', 'layout.njk');
   eleventyConfig.addDataExtension('yaml', (contents) => yaml.load(contents));
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.addFilter('md', parseMiniMd);
   eleventyConfig.addShortcode('image', responsiveImage);
+  eleventyConfig.addShortcode('icon', icon);
   eleventyConfig.addShortcode('license', license);
   eleventyConfig.addShortcode('photoAttribution', photoAttribution);
 
---
+++ src/js/shortcodes/icon.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+/**
+ * Display an icon.
+ *
+ * Find more at https://iconify.design/
+ *
+ * @param {string} icon filename in src/icons
+ * @param {string} alt alt text, used in aria-label
+ * @returns 
+ */
+module.exports = async function (icon, alt = '') {
+  const file = path.resolve('src/icons', icon + '.svg');
+  const svg = fs.readFileSync(file, 'utf8');
+  return alt ? `<div aria-label="${alt}">${svg}</div>` : `<div>${svg}</div>`;
+};
