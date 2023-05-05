ca340161ccef8c533a441cb2c60546665f3b5121
Emma Litwa-Vulcu <80431903+reiniiriarios@users.noreply.github.com>
2023-05-05 13:48:22 -0700

    fix: tab to spaces

--- src/js/filters/parse-mini-markdown.js
+++ src/js/filters/parse-mini-markdown.js
@@ -1,5 +1,5 @@
 module.exports = async function (text) {
-	const toHTML = text
+  const toHTML = text
     .replace(/^### (.*$)/gim, '<h3>$1</h3>') // h3 tag
     .replace(/^## (.*$)/gim, '<h2>$1</h2>') // h2 tag
     .replace(/^# (.*$)/gim, '<h1>$1</h1>') // h1 tag
